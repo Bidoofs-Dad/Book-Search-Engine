ee04eae2588ef45cd6b64280855afdf5f1f5ceb5
Zane Werle <zanewerle@gmail.com>
2023-08-03 15:42:43 -0500

    added in the useMutation method and replaced the saveBook function with the SAVE_BOOK mutation inside of searchbooks.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import {
   Container,
   Col,
@@ -11,6 +12,7 @@ import {
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -60,6 +62,9 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
+
+  const [saveBook, { error, data }] = useMutation(SAVE_BOOK);
+
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
@@ -72,11 +77,12 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      console.log('bookToSave:', bookToSave);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } }
+      });
+      console.log('data:', data);
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
@@ -121,7 +127,7 @@ const SearchBooks = () => {
         <Row>
           {searchedBooks.map((book, index) => {
             return (
-              <Col md="4" key={index}> 
+              <Col md="4" key={index}>
                 <Card key={book.bookId} border='dark'>
                   {book.image ? (
                     <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
