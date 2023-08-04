e8a11caba158412250f3784d45b0f91ec449a6cd
Zane Werle <zanewerle@gmail.com>
2023-08-04 12:23:43 -0500

    slight edits, taking out some curly brackets

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery, useMutation } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
+import  GET_ME  from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import {
   Container,
