91a774600fafdd1609bdf98a468c9ac4b8fb2a1c
Zane Werle <zanewerle@gmail.com>
2023-08-04 12:52:28 -0500

    trying a change in the connection to allow it to reach herooku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// || 'mongodb://127.0.0.1:27017/googlebooks',
 
 module.exports = mongoose.connection;
